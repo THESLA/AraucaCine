bfe7aa9a891544d12d9802c0375431642efb83fc
THESLA <THESLA@users.noreply.github.com>
2026-08-01 13:52:05 +0000

    deploy: 73a61166899c8f23037961316d1f1a20f3bf19b3

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'arauacine-v2';
+
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.origin !== location.origin) return;
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      fetch(e.request).catch(() => caches.match('/AraucaCine/index.html'))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }))
+  );
+});
